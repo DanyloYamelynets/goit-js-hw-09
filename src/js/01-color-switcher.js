5b275f3ed486cbe45dcf9e849f50cd51fdf8c4d6
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-06-14 22:45:07 +0300

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
-swdwdwdwdcldeded
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  colorsId = setInterval(onChangeColor, 1000);
+}
+
+function onStopClick() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(colorsId);
+}
+
+function onChangeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
