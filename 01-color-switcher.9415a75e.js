960cadcbbabbfc511e5a6742250bd445481ec39e
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-06-16 18:48:49 +0000

    Deploying to gh-pages from @ DanyloYamelynets/goit-js-hw-09@306a4c30a1d18131b7effa4dd0f475e43d1a3855 🚀

---
+++ 01-color-switcher.9415a75e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let n;function o(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,n=setInterval(o,1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.9415a75e.js.map
